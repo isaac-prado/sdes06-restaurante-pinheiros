cc07f9adc32b270956a9afc36ce2f24b42b838c9
Isaac do Prado Almeida <isaac.prado@tech4h.com.br>
2024-10-24 14:36:47 -0300

    feat: adds repo features

--- backend/src/WebUI/routers/clienteRouters.ts
+++ backend/src/WebUI/routers/clienteRouters.ts
@@ -1,11 +1,21 @@
 import express, { Router } from 'express';
+import criarCliente from '../../core/aplicacao/casosDeUso/clientes/criarCliente';
+import IClienteRepository from '../../core/aplicacao/contratos/iClienteRepository';
 const rotaBaseCliente: string = '/cliente';
 
 const clienteRouters: Router = express.Router();
 
-clienteRouters.post(rotaBaseCliente, (req, res) => {
-    var { nome, email, senha } = req.body;
-    res.send('Hello World');
+clienteRouters.post(rotaBaseCliente, async (req: Request<IClienteRepository>, res) => {
+    const { cpf, nome, endereco, telefone, email, saldo } = req.body;
+    
+    if(!cpf || !nome || !endereco || !telefone) {
+        return res.status(400).json({ message: "Dados obrigatórios não preenchidos"});
+    }
+
+    try {
+        const newCliente = await criarCliente({ cpf, nome, endereco, telefone, email, saldo })
+    }
+
 });
 
 
